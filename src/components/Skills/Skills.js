b7c41b6421e8d56c8935ab326d8f4f9fd6c1340d
Johan Girod <johangirod@gmail.com>
2018-01-22 13:05:36 +0100

    feat: add skill part content first draft

--- src/components/Skills/Skills.js
+++ src/components/Skills/Skills.js
@@ -1,5 +1,31 @@
 import React from 'react';
 
-const Skills = () => <section>H</section>;
+const Skills = () => (
+    <section>
+        <h2> Compétences </h2>
+        <h3>Expert</h3>
+        <ul>
+            {['React', 'Javascript', 'HTTP', 'Redux', 'Webpack'].map(talent => <li>{talent}</li>)}
+        </ul>
+        <h3>Compétent</h3>
+        <ul>
+            {[
+                'Node',
+                'Test Driven Developement',
+                'Flow',
+                'Jest',
+                'CSS',
+                'Babel',
+                'Programmation Fonctionnelle',
+            ].map(talent => <li>{talent}</li>)}
+        </ul>
+        <h3>Prochains talents</h3>
+        <ul>
+            {['Elm', 'Rust', 'Web assembly', 'CycleJS', 'ReactiveX'].map(talent => (
+                <li>{talent}</li>
+            ))}
+        </ul>
+    </section>
+);
 
 export default Skills;
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Header from '../components/Header/Header';
 import Résumé from '../components/Résumé/Résumé';
+import Skills from '../components/Skills/Skills';
 
 const Home = () => (
     <React.Fragment>
         <Header />
         <Résumé />
+        <Skills />
     </React.Fragment>
 );
 
