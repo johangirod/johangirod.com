d7e678dde26fa92dc6aaafd38ba64b7db81a58ec
Johan Girod <johangirod@gmail.com>
2018-01-25 17:05:46 +0100

    style: typography

--- src/utils/typography.js
+++ src/utils/typography.js
@@ -4,10 +4,11 @@ import Typography from 'typography';
 const newStyle = {
     ...style,
     headerFontFamily: ['Lora', 'serif'],
-    bodyFontFamily: ['Lato', 'sans-serif'],
+    bodyFontFamily: ['Merriweather Sans', 'sans-serif'],
     googleFonts: [
         { name: 'Lora', styles: ['400', '700'] },
-        { name: 'Lato', styles: ['400', '700'] },
+        { name: 'Merriweather Sans', styles: ['400', '700'] },
     ],
 };
+
 export default new Typography(newStyle);
