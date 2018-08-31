9f74e36326e8df7e2205915aba3af2f89e2b0d1f
Johan Girod <johangirod@gmail.com>
2018-08-31 15:17:15 +0200

    Fix typos

--- src/components/Résumé/Résumé.js
+++ src/components/Résumé/Résumé.js
@@ -21,22 +21,22 @@ const Résumé = () => (
             pendant des heures (surtout une bière à la main).
         </p>
         <p>
-            Une grosse affinité pour le <strong>produit</strong>. J’aime comprendre pourquoi je fait
-            les choses. J’aime <strong>comprendre le métier</strong>. J’aime être parti prenante
+            Une grosse affinité pour le <strong>produit</strong>. J’aime comprendre pourquoi je fais
+            les choses. J’aime <strong>comprendre le métier</strong>. J’aime être partie prenante
             dans l’élaboration du produit, avec le <strong>reste de la team</strong>. Je crois
             d’ailleurs que les meilleurs réalisations sont celles qui sont{' '}
-            <strong>co-construites</strong> avec toutes les compétences representée (Produit, UX,
+            <strong>co-construites</strong> avec toutes les compétences representées (Produit, UX,
             Front, Back).
         </p>
         <p>
-            Voilà maintenant <strong>{yearSince('2013-07-01')} ans</strong> que je fait du front,
+            Voilà maintenant <strong>{yearSince('2013-07-01')} ans</strong> que je fais du front,
             dont <strong>{yearSince('2015-12-01')} ans</strong> sur React uniquement.
         </p>
         <p>
             Auteur de <a href="https://github.com/Evaneos/vitaminjs">vitaminjs</a>
         </p>
         <p>
-            Je suis aussi un grand passioné de <a href="https://vagalam.bike"> voyage à vélo </a>
+            Je suis aussi un grand passionné de <a href="https://vagalam.bike"> voyage à vélo </a>
         </p>
     </section>
 );
